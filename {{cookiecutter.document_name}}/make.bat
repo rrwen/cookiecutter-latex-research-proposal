c5c9f9954a786105b9fafa4db6c5734ab2e48307
R W <rrwen.dev@gmail.com>
2019-02-16 21:59:38 -0500

    Use biber for references

--- {{cookiecutter.document_name}}/make.bat
+++ {{cookiecutter.document_name}}/make.bat
@@ -1,4 +1,4 @@
 xelatex {{cookiecutter.vars.file_name}} -output-directory=log -interaction=nonstopmode -halt-on-error
-bibtex log/{{cookiecutter.vars.file_name}} 
+biber log/{{cookiecutter.vars.file_name}} 
 xelatex {{cookiecutter.vars.file_name}}  -aux-directory=log -interaction=nonstopmode -halt-on-error
 xelatex {{cookiecutter.vars.file_name}}  -aux-directory=log -interaction=nonstopmode -halt-on-error
