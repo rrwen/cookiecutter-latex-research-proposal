7fb6055cdd6a579fadb1242dc9a2e110904fd9a8
R W <rrwen.dev@gmail.com>
2019-02-16 22:30:53 -0500

    Fix make.sh

--- {{cookiecutter.document_name}}/make.sh
+++ {{cookiecutter.document_name}}/make.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-xelatex {{cookiecutter.vars.file_name}} -interaction=nonstopmode -halt-on-error
-biber {{cookiecutter.vars.file_name}}
-xelatex {{cookiecutter.vars.file_name}} -interaction=nonstopmode -halt-on-error
-xelatex {{cookiecutter.vars.file_name}} -interaction=nonstopmode -halt-on-error
+mkdir -p log
+xelatex -output-directory=log -interaction=nonstopmode -halt-on-error {{cookiecutter.file_name}}
+biber log/{{cookiecutter.file_name}}
+xelatex -output-directory=log -interaction=nonstopmode -halt-on-error {{cookiecutter.file_name}}
+mv log/{{cookiecutter.file_name}}.pdf ./{{cookiecutter.file_name}}.pdf
